89d7910f73f8f55df42128dd5ff0edf515fde598
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2017-08-01 10:54:07 -0400

    Fixed

--- dataview.py
+++ dataview.py
@@ -4,7 +4,8 @@ import tflearn
 import matplotlib.pyplot as plt
 import scipy.misc
 import math
-
+import matplotlib.cbook as cbook
+from scipy.misc import imshow
 
 
 h5f = h5py.File('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/dataset.h5', 'r')
@@ -31,7 +32,7 @@ Y=np.zeros((Y1.shape[0],np.max(Y1)+1))
 for i in range(Y1.shape[0]):
 	Y[i,Y1[i]]=1
 
-print(Y)
+#print(Y)
 
 nframes=Y1.shape[0]
 
@@ -42,9 +43,20 @@ plt.ion()
 fig.show()
 fig.canvas.draw()
 
+X = np.squeeze(X)
+print(X.shape)
+image_file = cbook.get_sample_data('grace_hopper.png')
+image = plt.imread(image_file)
+
+fig, ax = plt.subplots()
+#im = ax.imshow(X[55,:,:])
+print("oeurggd")
+#ax.axis('off')
+plt.show()
+
 for i in range(nframes):
      print(i)
      print(Y1[i])
      ax.clear()
-     ax.imshow(X[i,:,:,:])
+     ax.imshow(X[i,:,:])
      fig.canvas.draw()
