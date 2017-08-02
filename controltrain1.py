befed37b16b2a0e868ab906c9cceb17d0cd19260
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2017-08-02 14:15:21 -0400

    Added in batch files

--- controltrain1.py
+++ controltrain1.py
@@ -100,6 +100,7 @@ class Data():
         h5f = h5py.File(dset_name, 'w')
         h5f.create_dataset('X', data=self.images)
         h5f.create_dataset('Y', data=self.actions)
+        h5f.create_dataset('Z', data=self.mph)
 
         h5f.close()
 
@@ -125,8 +126,8 @@ is_collecting = False
 terminal = False
 
 
-##Initialize # of frames
-frame_num = 100
+##Initialize # of frames in a batch
+frame_num = 1000
 
 ##-------------------------------------------------------##
 ##                     Starting                          ##
@@ -145,52 +146,70 @@ start = time.time()
 
 brightness_list = []
 
-for _ in range(frame_num):#while True:
-    frame_start = time.time()
-    keystates = get_keys(keystates) # Get keys that are currently pressed down, returns keystates dictionary
-    if keystates == 'terminal':
-        break
-    # Data collection on/off switch
-    if keystates['space'] and not last_frame_space:
-        # Turn data collection on/off
-        is_collecting = not is_collecting
+current_frame = 0
+current_batch = 0
+
+while True:
+
+    while current_frame < frame_num:
+        frame_start = time.time()
+        keystates = get_keys(keystates) # Get keys that are currently pressed down, returns keystates dictionary
+        if keystates == 'terminal':
+            break
+        # Data collection on/off switch
+        if keystates['space'] and not last_frame_space:
+            # Turn data collection on/off
+            is_collecting = not is_collecting
+            if is_collecting:
+                print("IS NOW COLLECTING DATA")
+            else:
+                print("STOPPED COLLECTING DATA")
+        last_frame_space = keystates['space']
+
+        keystates_array = keystates.values() # Converts keystates into an array
+        keystates_array = np.asarray(keystates_array) + 0.0 # Converts into Numpy array of 0's and 1's
+        keystates_array = keystates_array[:,None] # Adds an extra dimension
+        keystates_array = np.transpose(keystates_array) # Transposes array
+
+        cam.wait_for_frames() # This gets camera input stream as cam.color array
+        # Create h5py file here, containing the numpy array and the array keystates_array
+        #c = color.rgb2gray(cam.color)
+        c = np.mean(cam.color, 2)
+        e = c
+        c = resize(c, (240,320))
+        c = np.asarray(c)
+        c = c[None, :, :, None]
+
+        im = Image.fromarray(cam.color)
+        cropped_mph_im = im.crop(crop_mph)
+        mph = np.full((1),curve_to_mph(cropped_mph_im, br_thresh))
+
         if is_collecting:
-            print("IS NOW COLLECTING DATA")
-        else:
-            print("STOPPED COLLECTING DATA")
-    last_frame_space = keystates['space']
+            print("Is collecting...")
+            d.images = np.concatenate((d.images, c))
+            d.actions = np.concatenate((d.actions, keystates_array))
+            d.mph = np.concatenate((d.mph, mph))
+            current_frame += 1
+        send_keys(board, keystates) #Send appropriate keystrokes from keystates through the arduino
+        elapsed_frame = time.time()-frame_start
+        print("Elapsed time: %s seconds" % elapsed_frame)
 
-    keystates_array = keystates.values() # Converts keystates into an array
-    keystates_array = np.asarray(keystates_array) + 0.0 # Converts into Numpy array of 0's and 1's
-    keystates_array = keystates_array[:,None] # Adds an extra dimension
-    keystates_array = np.transpose(keystates_array) # Transposes array
-
-    cam.wait_for_frames() # This gets camera input stream as cam.color array
-    # Create h5py file here, containing the numpy array and the array keystates_array
-    #c = color.rgb2gray(cam.color)
-    c = np.mean(cam.color, 2)
-    e = c
-    c = resize(c, (240,320))
-    c = np.asarray(c)
-    c = c[None, :, :, None]
-
-    im = Image.fromarray(cam.color)
-    cropped_mph_im = im.crop(crop_mph)
-    mph = np.full((1),curve_to_mph(cropped_mph_im, br_thresh))
-
-
-    if is_collecting:
-        print("Is collecting...")
-        d.images = np.concatenate((d.images, c))
-        d.actions = np.concatenate((d.actions, keystates_array))
-        d.mph = np.concatenate((d.mph, mph))
-
-
-    send_keys(board, keystates) #Send appropriate keystrokes from keystates through the arduino
-    elapsed_frame = time.time()-frame_start
-    print("Elapsed time: %s seconds" % elapsed_frame)
-
-d.save('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/dataset.h5')
+    print("Batch %s complete and saving" % current_batch)
+    d.save('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/dataset%s.h5' % current_batch)
+    print("Saved. Press the 1 key (top of the keyboard) to continue on next batch...")
+    while True:
+        next_batch = False
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    next_batch = True
+        if next_batch == True:
+            break
+    current_batch += 1
+    current_frame = 0
+    is_collecting = False
+    next_batch = False
+    print("Starting batch %s ..." % current_batch)
 
 elapsed = time.time()-start
 fps = frame_num/elapsed
