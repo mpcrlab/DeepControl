a35f8f9a44f4077de2cfd9834b7355e315e75b2e
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2017-08-08 11:49:46 -0400

    goes through all .h5 files

--- deleteblankframes.py
+++ deleteblankframes.py
@@ -1,3 +1,6 @@
+# controltrain1.py leaves some blank frames in every .h5 file,
+# so this program deletes them
+
 import numpy as np
 import h5py
 import tflearn
@@ -23,13 +26,13 @@ def is_number(s):
         pass
     return False
 
-
 data_files = []
 
+# Get all filenames of datasets into a list
 for (dirpath, dirnames, filenames) in walk('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset'):
     data_files.extend(filenames)
     break
-
+# Extract the dataset numbers
 for i in range(len(data_files)):
     if is_number(data_files[i][8]):
         data_files[i] = int(data_files[i][7] + data_files[i][8])
@@ -39,83 +42,65 @@ data_files.sort()
 
 print(data_files)
 
-h5f = []
+for set_num in range(len(data_files)):
+    h5f = []
+    print("Deleting blank frames from dataset %s" % set_num)
+    h5f.append(h5py.File('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/dataset' + str(data_files[set_num]) + '.h5'))
 
-for i in range(len(data_files)):
-    x = raw_input("Append %s? (Y/N): " % i)
-    if x == "Y" or x == "y":
-        h5f.append(h5py.File('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/dataset' + str(data_files[i]) + '.h5'))
-    else:
-        pass
+    image_set = np.asarray(h5f[0]['X'])
+    action_array_set = np.asarray(h5f[0]['Y'])
+    action_array_set = action_array_set.astype(int)
+    mph = np.asarray(h5f[0]['Z'])
+    mph = mph.astype(int)
 
+    nframes = action_array_set.shape[0]
 
-image_set = np.asarray(h5f[0]['X'])
-action_array_set = np.asarray(h5f[0]['Y'])
-action_array_set = action_array_set.astype(int)
-mph = np.asarray(h5f[0]['Z'])
-mph = mph.astype(int)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.ion()
 
-if len(h5f) > 1:
-    for i in range(1,len(h5f)):
-        print(i)
-        image_set = np.concatenate((image_set, np.asarray(h5f[i]['X'])))
-        action_array_set = np.concatenate((action_array_set, np.asarray(h5f[i]['Y'])))
-        action_array_set = action_array_set.astype(int)
-        mph = np.concatenate((mph, h5f[i]['Z']))
-        mph = mph.astype(int)
-
-
-nframes = action_array_set.shape[0]
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.ion()
-
-fig.show()
-fig.canvas.draw()
-
-image_set = np.squeeze(image_set)
-
-def return_keys(keystates):
-    key_string = ""
-    for i in range(len(keystates)):
-        if keystates[i] == 1.:
-            if i == 0:
-                key_string += "right-"
-            if i == 1:
-                key_string += "down-"
-            if i == 2:
-                key_string += "shift-"
-            if i == 3:
-                key_string += "up-"
-            if i == 4:
-                key_string += "down-"
-            if i == 5:
-                key_string += "left-"
-    return key_string
-print(image_set.shape)
-
-for i in range(nframes):
-    ax.clear()
-    print(np.sum(image_set[i,:,:]))
-    print("frame " + str(i))
-    ax.imshow(image_set[i,:,:])
-    if np.sum(image_set[i,:,:]) == 0.0:
-        print(image_set.shape)
-        image_set = np.delete(image_set, i, axis=0)
-        print("Removed!, Shape: %s" % (str(image_set.shape)))
-    key_string = return_keys(action_array_set[i])
-    print(action_array_set)
+    fig.show()
     fig.canvas.draw()
 
-print(image_set.shape)
-print(action_array_set.shape)
-h5f = h5py.File('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/noblanks0.h5', 'w')
-h5f.create_dataset('X', data=image_set)
-h5f.create_dataset('Y', data=action_array_set)
-h5f.create_dataset('Z', data=mph)
+    image_set = np.squeeze(image_set)
 
-h5f.close()
+    def return_keys(keystates):
+        key_string = ""
+        for i in range(len(keystates)):
+            if keystates[i] == 1.:
+                if i == 0:
+                    key_string += "right-"
+                if i == 1:
+                    key_string += "down-"
+                if i == 2:
+                    key_string += "shift-"
+                if i == 3:
+                    key_string += "up-"
+                if i == 4:
+                    key_string += "down-"
+                if i == 5:
+                    key_string += "left-"
+        return key_string
+    print(image_set.shape)
 
-#103, 204, 305
-#406, 507, 610, 711, 812, 913, 1014, 1015, 1016
+    for i in range(nframes):
+        ax.clear()
+        print(np.sum(image_set[i,:,:]))
+        print("frame " + str(i))
+        ax.imshow(image_set[i,:,:])
+        if np.sum(image_set[i,:,:]) == 0.0:
+            print(image_set.shape)
+            image_set = np.delete(image_set, i, axis=0)
+            print("Removed!, Shape: %s" % (str(image_set.shape)))
+        key_string = return_keys(action_array_set[i])
+        print(action_array_set)
+        fig.canvas.draw()
+
+    print(image_set.shape)
+    print(action_array_set.shape)
+    h5f = h5py.File('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset/fixed/dataset%s.h5' % set_num, 'w')
+    h5f.create_dataset('X', data=image_set)
+    h5f.create_dataset('Y', data=action_array_set)
+    h5f.create_dataset('Z', data=mph)
+
+    h5f.close()
