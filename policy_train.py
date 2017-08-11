a542547db96560f13b5baac7175b196507ce4161
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2017-08-11 11:33:12 -0400

    changed one-hot conversion, output visualization

--- policy_train.py
+++ policy_train.py
@@ -19,6 +19,7 @@ import sys
 import os
 import cv2
 from networkswitch import *
+import operator
 from sklearn.preprocessing import scale
 
 
@@ -35,13 +36,13 @@ os.chdir('/home/mpcr/Desktop/rodrigo/deepcontrol/dataset')
 fnames = glob.glob('*.h5') # datasets to train on
 fnames.sort(key=lambda f: int(filter(str.isdigit, f)))
 epochs = 5000 # number of training iterations
-batch_sz = 30  # training batch size
+batch_sz = 200  # training batch size
 test_num = 650  # Number of validation examples
 f_int = 5 # for framestack
 f_int2 = 15 # for framestack
 val_accuracy = [] # variable to store the validation accuracy
 num_stack = 1
-val_name = 'dataset31.h5' # Dataset to use for validation
+val_name = 'dataset30.h5' # Dataset to use for validation
 num_iters = 0.
 num_classes = 20
 binary = False # Binary crossentropy or not
@@ -83,6 +84,13 @@ def batch_get(filename, batch_size):
     #Y[np.arange(batch_sz), y[rand]] = 1.0 # create one-hot label vector
     #X = np.mean(X[rand, 110:, :, :], 3, keepdims=True) # grayscale and crop frames
     #assert(X.shape[0] == Y.shape[0]), 'Data and labels different sizes'
+
+    # Convert labels to one-hot vectors
+    Z = np.zeros((batch_sz, num_classes))
+    if binary == False:
+        for j in range(len(Y)):
+            Z[j] = combo_to_onehot(Y[j])
+    Y = Z
     f.flush()
     f.close()
     return X, Y
@@ -142,7 +150,7 @@ merged = tf.summary.merge_all()
 
 
 # gradient descent optimizer
-opt = tf.train.AdamOptimizer(learning_rate=0.0001)
+opt = tf.train.AdamOptimizer(learning_rate=0.000033)
 trainop = tflearn.TrainOp(loss=cost,
                          optimizer=opt,
                          metric=None,
@@ -154,8 +162,11 @@ writer = tf.summary.FileWriter('/tmp/tflearn_logs/test'+m_save+modelswitch[model
 writer2 = tf.summary.FileWriter('/tmp/tflearn_logs/train'+m_save+modelswitch[model_num].__name__,
                                model.session.graph)
 
-for i in range(epochs):
-    print(i)
+out_log = []
+i = -1
+while i < epochs:
+    i += 1
+    print("Epoch: %s" % i)
     n = np.random.randint(0, len(fnames)-1, 1) # draw a random integer from 1 to # of files
     filename = fnames[n[0]] # name file according to the random number index
     # skip validation set if chosen
@@ -175,24 +186,26 @@ for i in range(epochs):
     # Data Augmentation
     #X, Y = add_noise(X, Y)
 
-    # Convert labels to one-hot vectors
-    Z = np.zeros((batch_sz, num_classes))
-    if binary == False:
-        for i in range(len(Y)):
-            Z[i] = combo_to_onehot(Y[i])
-    Y = Z
-
     # Training
     model.fit_batch(feed_dicts={network:X, labels:Y})
+
+    #Visualize output
+    out = model.session.run(net_out, feed_dict={network:X, labels:Y})
+    out_ = []
+    for j in range(out.shape[1]):
+        out_.append(out[0][j])
+    out = out_
+    out_log.append(out)
+
     train_acc, train_loss = model.session.run([acc, cost], feed_dict={network:X, labels:Y})
 
     train_summary = model.session.run(merged, feed_dict={network:X, labels:Y})
     writer2.add_summary(train_summary, i)
 
     if i%50 == 0:
+        print("Validating")
         # get validation batch
-        tx, ty = batch_get(val_name, 600)
-
+        tx, ty = batch_get(val_name, 500)
         # feature scale validation data
         tx = feature_scale(tx)
 
@@ -206,6 +219,8 @@ for i in range(epochs):
                                                    feed_dict={network:tx, labels:ty})
         writer.add_summary(summary, i)
 
-
 # Save model and acc/error curves
 model.save(m_save+modelswitch[model_num].__name__)
+
+# Save model output throughout training
+os.chdir('/home/mpcr/Desktop/rodrigo/deepcontrol')
