0035e01574f51aeda284428eaa9a1993950e981a
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2017-08-15 15:31:41 -0400

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -20,15 +20,18 @@ WHITE    = ( 255, 255, 255)
 
 # Initialize PyGame
 pygame.init()
-size = [500, 500]
+size = [750, 750]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Spinning Dial")
 clock = pygame.time.Clock()
 
 class Pointer(object):
-    def __init__(self,pointer_im,x=250,y=250):
+    def __init__(self,pointer_im,dial_im,x=250,y=250):
         self.x = x
         self.y = y
+        self.angle = 135
+        self.angle_minmax = (-45,135)
+        self.dial_im = dial_im
         self.pointer_im = pointer_im
         self.pointer_im_rect = self.pointer_im.get_rect()
     def rotate(self, angle):
@@ -38,12 +41,19 @@ class Pointer(object):
 
         return rot_pointer_im, rot_pointer_rect
 
-pointer1 = Pointer(pygame.image.load("pointer.png").convert_alpha(), 200, 200)
+class Slider(object):
+    def __init__(self,rectangle_im,slider_im,x=500,y=250):
+        self.x = x
+        self.y = y
+        self.rectangle_im = rectangle_im
+        self.slider_im = slider_im
+        self.value = 0
+        self.value_minmax = (0,100)
 
-dial1 = pygame.image.load("dial.gif").convert_alpha()
 
-angle = 0
-angle_minmax = (-45,135)
+pointer1 = Pointer(pygame.image.load("pointer.png").convert_alpha(),pygame.image.load("dial.gif").convert_alpha(), 200, 200)
+slider1 = Slider(pygame.image.load("sliderrectangle1.png").convert_alpha(),pygame.image.load("bar.jpg").convert_alpha())
+
 keystates={'up':False, 'down':False}
 done = False
 
@@ -52,20 +62,25 @@ while not done:
         keystates = get_keys(keystates)
         if event.type == pygame.QUIT:
             done = True
-    if keystates['up'] and not angle > angle_minmax[1]:
-        angle += 10
-        print(angle)
-    if keystates['down'] and not angle < angle_minmax[0]:
-        angle -= 10
-        print(angle)
+    if keystates['down'] and not slider1.value <= 0:
+        slider1.value -= 5
+        pointer1.angle = (-9 * slider1.value)/5 + 135
+        print(slider1.value)
+    if keystates['up'] and not slider1.value >= 100:
+        slider1.value += 5
+        pointer1.angle = (-9 * slider1.value)/5 + 135
+        print(slider1.value)
 
     # Rotating image
-    rot_pointer_im, rot_pointer_rect = pointer1.rotate(angle)
+    rot_pointer_im, rot_pointer_rect = pointer1.rotate(pointer1.angle)
 
-    # Blit rotated image
+    # Blit Dial
     screen.fill(WHITE)
-    screen.blit(dial1, (pointer1.x-50,pointer1.y-50))
+    screen.blit(pointer1.dial_im, (pointer1.x-50,pointer1.y-50))
     screen.blit(rot_pointer_im, rot_pointer_rect)
+
+    screen.blit(slider1.slider_im, (slider1.x, (slider1.y + 30) - slider1.value/3))
+    screen.blit(slider1.rectangle_im, (slider1.x,slider1.y))
     pygame.display.flip()
     clock.tick(20)
 
