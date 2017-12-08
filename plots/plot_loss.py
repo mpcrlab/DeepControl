6f70473d053b0cb62eee935c5887026af55099c3
Rodrigo Castellon <rodrigo.castellon@pinecrest.edu>
2017-12-08 17:54:07 -0500

    Create plot_loss.py

---
+++ plots/plot_loss.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.signal as sign
+
+data = pd.read_json('run-train12-1-withstraightrecovery3Alex1-tag-Loss_.json')
+
+yhat = sign.savgol_filter(data[2], 15, 3)
+plt.plot(data[1],yhat, color='black', linewidth=0.5)
+plt.title("Error/Loss of CNN throughout the training process")
+plt.ylabel("Loss (%)")
+plt.xlabel("Training epochs")
+plt.show()
