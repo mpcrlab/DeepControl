2d083c3fa57e4e1ef9d42ea354fd69c2b962ae52
Rodrigo Castellon <rodrigo.castellon@pinecrest.edu>
2017-12-15 11:45:16 -0500

    Update controlfunctions.py

--- controlfunctions.py
+++ controlfunctions.py
@@ -194,6 +194,153 @@ def send_keys(board, keystates):
             board.digitalWrite(11, "LOW")
 
         # print("---------------------\n")
+    return 1
+
+def curve_to_mph(im, br_thresh):
+    width, height = im.size
+    im_rects = []
+    for i in range(10):
+        im_rects.append(im.crop((i*19,0,(i+1)*19,height)))
+    stat = [ImageStat.Stat(im).mean[0] for im in im_rects]
+    return dropoff(stat, br_thresh)
+
+def get_keys(keystates):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            return 'terminal'
+        keys = pygame.key.get_pressed()
+        #check for key down events
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_UP:
+                keystates['up']=True
+            if event.key == K_DOWN:
+                keystates['down']=True
+            if event.key == K_LEFT:
+                keystates['left']=True
+            if event.key == K_RIGHT:
+                keystates['right']=True
+            if event.key == K_LSHIFT:
+                keystates['shift']=True
+            if event.key == K_SPACE:
+                keystates['space']=True
+
+        #check for key up events
+        if event.type == pygame.KEYUP:
+            if event.key == K_UP:
+                keystates['up']=False
+            if event.key == K_DOWN:
+                keystates['down']=False
+            if event.key == K_LEFT:
+                keystates['left']=False
+            if event.key == K_RIGHT:
+                keystates['right']=False
+            if event.key == K_LSHIFT:
+                keystates['shift']=False
+            if event.key == K_SPACE:
+                keystates['space']=False
+    return keystates
+
+def send_keys(board, keystates):
+    if isinstance(keystates, dict):
+        #do something about the key states here, now that the event queue has been processed
+        if 'enter' in keystates.values():
+            if keystates['enter']:
+                print("enter on")
+                board.digitalWrite(1, "HIGH")
+                board.digitalWrite(1, "LOW")
+                time.sleep(2)
+            else:
+                board.digitalWrite(1, "LOW")
+
+
+        if keystates['up']:
+            #print("Up")
+            board.digitalWrite(9, "HIGH")
+        else:
+            board.digitalWrite(9, "LOW")
+
+
+        if keystates['down']:
+            #print("Down")
+            board.digitalWrite(7, "HIGH")
+        else:
+            board.digitalWrite(7, "LOW")
+
+
+        if keystates['left'] and keystates['shift']:
+            # print("Hard left")
+            board.digitalWrite(3, "HIGH")
+        else:
+            board.digitalWrite(3, "LOW")
+
+
+        if keystates['left'] and not keystates['shift']:
+            # print("Left")
+            board.digitalWrite(5, "HIGH")
+        else:
+            board.digitalWrite(5, "LOW")
+
+
+        if keystates['right'] and keystates['shift']:
+            # print("Hard right")
+            board.digitalWrite(13, "HIGH")
+        else:
+            board.digitalWrite(13, "LOW")
+
+
+        if keystates['right'] and not keystates['shift']:
+            # print("Right")
+            board.digitalWrite(11, "HIGH")
+        else:
+            board.digitalWrite(11, "LOW")
+
+        #print("---------------------\n")
+
+    else:
+        #do something about the key states here, now that the event queue has been processed
+        if keystates[0][3]:
+            # print("Up")
+            board.digitalWrite(9, "HIGH")
+        else:
+            board.digitalWrite(9, "LOW")
+
+
+        if keystates[0][0]:
+            # print("Down")
+            board.digitalWrite(7, "HIGH")
+        else:
+            board.digitalWrite(7, "LOW")
+
+
+        if keystates[0][4] and keystates[0][1]:
+            # print("Hard left")
+            board.digitalWrite(3, "HIGH")
+        else:
+            board.digitalWrite(3, "LOW")
+
+
+        if keystates[0][4] and not keystates[0][1]:
+            # print("Left")
+            board.digitalWrite(5, "HIGH")
+        else:
+            board.digitalWrite(5, "LOW")
+
+
+        if keystates[0][2] and keystates[0][1]:
+            # print("Hard right")
+            board.digitalWrite(13, "HIGH")
+        else:
+            board.digitalWrite(13, "LOW")
+
+
+        if keystates[0][2] and not keystates[0][1]:
+            # print("Right")
+            board.digitalWrite(11, "HIGH")
+        else:
+            board.digitalWrite(11, "LOW")
+
+        # print("---------------------\n")
                 keystates['right']=True
             if event.key == K_LSHIFT:
                 keystates['shift']=True
